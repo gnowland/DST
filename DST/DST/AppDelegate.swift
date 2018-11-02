75f697e843c4cefcf19750dc8982ca48bfd405c2
Gifford Nowland <hi@giffordnowland.com>
2018-11-01 23:18:08 -0700

    Create new Xcode project

---
+++ DST/DST/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DST
+//
+//  Created by Gifford Nowland on 11/1/18.
+//  Copyright © 2018 Gifford Nowland. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
